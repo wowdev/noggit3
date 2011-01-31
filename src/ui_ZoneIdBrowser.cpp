1bef37aa6d63fae4319744fc20ca456861577822
Steff <project.modcraft@googlemail.com>
2011-01-31 01:52:35 +0100

    Add list of AreaIds to the listview.

--- src/ui_ZoneIdBrowser.cpp
+++ src/ui_ZoneIdBrowser.cpp
@@ -7,19 +7,21 @@
 #include "scrollbarUI.h"
 #include "textUI.h" // textUI
 #include "noggit.h" // arial14, arialn13
+#include "dbc.h"
 
 ui_ZoneIdBrowser::ui_ZoneIdBrowser(int xPos,int yPos, int w, int h, Gui *setGui) : window(xPos,yPos,w,h)
 {
 	this->mainGui = setGui;
-	this->ZoneIdList = new ui_ListView(4,20,w - 8,h - 26,20);
+	this->ZoneIdList = new ui_ListView(4,4,w - 8,h - 8,20);
 	this->ZoneIdList->clickable = true;
-	for(int i = 1 ; i <= 60 ; i++)
+	
+
+	for( DBCFile::Iterator i = gAreaDB.begin(); i != gAreaDB.end(); ++i ) 
 	{
 		frame *curFrame = new frame(1,1,1,1); 
 		std::stringstream ss;
-		ss << "Eintrag Nummer: " << i;
-
-		curFrame->addChild(new textUI( 200.0f, 20.0f, ss.str() , &arialn13, eJustifyLeft ) );
+		ss << i->getInt(AreaDB::AreaID) << "-" << gAreaDB.getAreaName(i->getInt(AreaDB::AreaID));
+		curFrame->addChild(new textUI( 0.0f, 5.0f, ss.str() , &arialn13, eJustifyLeft ) );
 		this->ZoneIdList->addElement(curFrame);
 	}
 
