c855042ad0ce92e9913944a4e01989f5cf81afe4
Steff <Steff@localhost>
2011-01-31 20:23:48 +0100

    Fix some psmale placement bug with the scroll knob. Now the listView stop to scroll if the last entry is on the bottom of the list. Not if it is on the top and the list is empty except the last entry. Add function to set the mapid of the ZeonIDBrowser. Put the fill routine for the list in a seperate function. Ad setMapId function. Browser shows now only the AreaIDs depending on the current loaded map. Add mathod to world that return the current MapID. Since the last changes noggit crash - 80%. Don?t know why. Water perhaps :)

--- src/ui_ZoneIdBrowser.h
+++ src/ui_ZoneIdBrowser.h
@@ -9,9 +9,13 @@ class ui_ZoneIdBrowser : public window
 {
 public:
 	ui_ZoneIdBrowser(int xPos,int yPos, int w, int h, Gui *setGui);
+	void setMapID(int id);
+
 private:
 		Gui *mainGui;
 		ui_ListView *ZoneIdList;
+		int mapID;
+		void buildAreaList();
 };
 
 #endif
