b9f2ba5af83e843a29b719d8d47dcd66c65360c8
Steff <Steff@localhost>
2011-01-31 01:34:28 +0100

    Impement the basic list view. Some changes on scrollbar to implement the changeFunc.

--- src/ui_ListView.cpp
+++ src/ui_ListView.cpp
@@ -1,36 +1,60 @@
 #include "ui_ListView.h"
 #include "scrollbarUI.h"
+#include "log.h"
+#include "misc.h"
 
-ui_ListView::ui_ListView(float xPos, float yPos, float w, float h) : frame(xPos,yPos,w,h)
+void changeValue(frame *f,int set)
 {
-	scrollbarUI *scrollbar = new scrollbarUI(w-22,5,h-10,100);
-	scrollbar->clickable = true;
-	this->addChild(scrollbar);
+	((ui_ListView *)(f->parent))->recalcElements(set+1);
 }
 
+ui_ListView::ui_ListView(float xPos, float yPos, float w, float h, int elementHeight) : frame(xPos,yPos,w,h)
+{
+	this->elements_height = elementHeight;
+	this->scrollbar = new scrollbarUI(w-22,5,h-10,0);
+	this->scrollbar->clickable = true;
+	this->scrollbar->setChangeFunc(changeValue);
+	this->addChild(scrollbar);
+	this->elements_rows = (int)( h / elementHeight);
+}
 
 ui_ListView::~ui_ListView(void)
 {
 }
 
-void ui_ListView::addElement( frame element )
+void ui_ListView::addElement( frame *element )
 {
-	element.height = this->elements_height;
-	element.width = this->width-20;
-	this->addChild(&element);
-}
-
-void ui_ListView::delElement( int num )
-{
-
-}
-
-void ui_ListView::setElementsHeight( int h )
-{
-	this->elements_height = h;
+	element->height = this->elements_height;
+	element->width = this->width-20;
+	this->addChild(element);
+	this->scrollbar->setNum(this->children.size()-1);
+	recalcElements(1);
 }
 
 int ui_ListView::getElementsCount()
 {
 	return this->children.size();
+}
+
+void ui_ListView::delElement( int num )
+{
+	//recalcElements();
+}
+
+void ui_ListView::recalcElements(int value)
+{
+	this->elements_start = value;
+	// recalculate the position and the hide value off all child.
+	int rowCount = 0;
+	for(unsigned int i=1;i<children.size();++i)
+	{
+		if(i >= value && i < value + this->elements_rows)
+		{
+			// elements in the view block
+			children[i]->y = rowCount * this->elements_height;
+			children[i]->hidden = false;
+			rowCount++;
+		}
+		else children[i]->hidden = true;
+	}
 }
--- src/ui_ListView.h
+++ src/ui_ListView.h
@@ -2,19 +2,24 @@
 #define __UILISTVIEW_H
 
 #include "frame.h"
+#include "scrollbarUI.h"
 
 class ui_ListView : public frame
 {
 public:
-	ui_ListView(float xPos, float yPos, float w, float h);
+	ui_ListView(float xPos, float yPos, float w, float h, int elementHeight);
 	~ui_ListView(void);
-	void addElement(frame element);
+	void addElement(frame *element);
 	void delElement(int num);
 	void setElementsHeight(int h);
 	int	 getElementsCount();
+	void recalcElements(int value);
 private:
 	int elements_height;
 	int elements_start;
+	int elements_rows;
+	scrollbarUI *scrollbar;
+
 };
 
 #endif
