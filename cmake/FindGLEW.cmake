dd82ab33340c233ce08393472def75b68d8ac903
Steff <project.modcraft@googlemail.com>
2011-09-01 22:37:13 +0200

    CMake Files: Update the cmake files to work for me on windows.

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -1,55 +1,47 @@
-# - Try to find GLEW
+#
+# Try to find GLEW library and include path.
 # Once done this will define
-#  
-#  GLEW_FOUND        - system has GLEW
-#  GLEW_INCLUDE_DIR  - the GLEW include directory
-#  GLEW_LIBRARY_DIR  - where the libraries are
-#  GLEW_LIBRARY      - Link these to use GLEW
-#   
+#
+# GLEW_FOUND
+# GLEW_INCLUDE_PATH
+# GLEW_LIBRARY
+# 
 
-IF (GLEW_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(GLEW_FIND_QUIETLY TRUE)
-ENDIF (GLEW_INCLUDE_DIR)
+IF (WIN32)
+	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+		$ENV{PROGRAMFILES}/GLEW/include
+		${PROJECT_SOURCE_DIR}/src/nvgl/glew/include
+		DOC "The directory where GL/glew.h resides")
+	FIND_LIBRARY( GLEW_LIBRARY
+		NAMES glew GLEW glew32 glew32s
+		PATHS
+		$ENV{PROGRAMFILES}/GLEW/lib
+		${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
+		${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
+		DOC "The GLEW library")
+ELSE (WIN32)
+	FIND_PATH( GLEW_INCLUDE_PATH GL/glew.h
+		/usr/include
+		/usr/local/include
+		/sw/include
+		/opt/local/include
+		DOC "The directory where GL/glew.h resides")
+	FIND_LIBRARY( GLEW_LIBRARY
+		NAMES GLEW glew
+		PATHS
+		/usr/lib64
+		/usr/lib
+		/usr/local/lib64
+		/usr/local/lib
+		/sw/lib
+		/opt/local/lib
+		DOC "The GLEW library")
+ENDIF (WIN32)
 
-if( WIN32 )
-   if( MSVC80 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ 8/VC/PlatformSDK" )
-   endif( MSVC80 )
-   if( MSVC90 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ 9.0/VC" )
-   endif( MSVC90 )
+IF (GLEW_INCLUDE_PATH)
+	SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
+ELSE (GLEW_INCLUDE_PATH)
+	SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
+ENDIF (GLEW_INCLUDE_PATH)
 
-   if( MSVC71 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ .NET\ 2003/Vc7/PlatformSDK" )
-   endif( MSVC71 )
-   FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h GL/wglew.h
-              PATHS c:/glew/include ${COMPILER_PATH}/include )
-   SET( GLEW_NAMES glew32 )
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS c:/glew/lib ${COMPILER_PATH}/Lib )
-else( WIN32 )
-   FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
-              PATHS /usr/local/include /usr/include
-              PATH_SUFFIXES gl/ GL/ )
-   SET( GLEW_NAMES glew GLEW )
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS /usr/lib /usr/local/lib )
-endif( WIN32 )
-
-GET_FILENAME_COMPONENT( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} PATH )
-
-IF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET(GLEW_FOUND TRUE)
-    SET( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} )
-ELSE (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET( GLEW_FOUND FALSE )
-   SET( GLEW_LIBRARY_DIR )
-ENDIF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLEW DEFAULT_MSG GLEW_LIBRARY GLEW_INCLUDE_DIR)
-
-MARK_AS_ADVANCED(GLEW_INCLUDE_DIR GLEW_LIBRARY)
+MARK_AS_ADVANCED( GLEW_FOUND )
