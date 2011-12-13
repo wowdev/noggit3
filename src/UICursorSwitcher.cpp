82939b1c5938ad5e446795c2a6d930fa28e729f0
glararan <glararan@glararan.eu>
2011-12-13 18:04:56 +0100

    Fix: CursorSwitcher - Now has cool window width and heigh + correct position of Checkboxs Added: Disk cursor Added: Now can be used CursorSwitcher Error: Need repair Buttons in Cursor Switcher typo: bad checks

--- src/UICursorSwitcher.cpp
+++ src/UICursorSwitcher.cpp
@@ -16,12 +16,11 @@ void SwitchDisk(bool /*f*/, int /*id*/)
 	cursorDisk = true;
 }
 
-float Wwidth = 765;
-float Wheight = 400;
+float Wwidth = 480;
+float Wheight = 320;
 
-//UICursorSwitcher::UICursorSwitcher(float x, float y, float w, float h) : UICloseWindow(x, y, w, h, "Select the cursor you want to switch.", true)
 UICursorSwitcher::UICursorSwitcher() : UICloseWindow((float)video.xres() / 2.0f - Wwidth / 2.0f, (float)video.yres() / 2.0f - Wheight / 2.0f, Wwidth, Wheight, "Select the cursror you want to switch.", true)
 {
 	addChild(new UICheckBox(30.0f, 30.0f, "Cursor - Disk", SwitchDisk, 0));
-	addChild(new UICheckBox(90.0f, 30.0f, "Cursor - Sphere", SwitchSphere, 0));
+	addChild(new UICheckBox(150.0f, 30.0f, "Cursor - Sphere", SwitchSphere, 0));
 }
