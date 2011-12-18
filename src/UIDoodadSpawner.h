7118d4322426fabad262b7b89bd20a80e4acf6a8
Bernd Lo?rwald <bloerwald@googlemail.com>
2011-12-18 13:27:10 +0100

    Add a tree view to UIDoodadSpawner to add models by clicking.

--- src/UIDoodadSpawner.h
+++ src/UIDoodadSpawner.h
@@ -4,12 +4,14 @@
 #include "UICloseWindow.h"
 #include "UITextBox.h"
 #include "UIButton.h"
+#include "UITreeView.h"
 
 class UIDoodadSpawner : public UICloseWindow
 {
 private:
   UITextBox::Ptr _tbox;
   UIButton::Ptr _button;
+  UITreeView::Ptr _treeView;
 
 public:
   UIDoodadSpawner();
