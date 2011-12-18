53701678e8f91e1dbe9d9fed65a7aa22ccfb8cca
Bernd Lörwald <bloerwald@googlemail.com>
2011-12-18 13:27:10 +0100

    Add a tree view to UIDoodadSpawner to add models by clicking.

--- src/UIDoodadSpawner.cpp
+++ src/UIDoodadSpawner.cpp
@@ -11,10 +11,19 @@
 #include "WMOInstance.h" // WMOInstance
 #include "World.h"
 
+#include "Log.h"
+
 // TODO : Add TreeView. Add ScrollBar. Add ModelPreview
 
 static const float winWidth( 500.0f );
 static const float winHeight( 100.0f );
+
+static UIDoodadSpawner* global_doodadSpawner_evil( NULL );
+
+void UIDoodadSpawner__TreeSelect( const std::string& value )
+{
+  global_doodadSpawner_evil->AddM2( value );
+}
 
 void UIDoodadSpawner__TextBoxEnter( UITextBox::Ptr textBox, const std::string& value )
 {
@@ -27,13 +36,36 @@ void AddM2Click( UIFrame* f, int i )
   ( static_cast<UIDoodadSpawner *>( textBox->parent() ) )->AddM2( textBox->value() );
 }
 
+extern std::list<std::string> gListfile;
+
 UIDoodadSpawner::UIDoodadSpawner( )
 : UICloseWindow( video.xres() / 2.0f - winWidth / 2.0f, video.yres() / 2.0f - winHeight / 2.0f, winWidth, winHeight, "Test", true )
 , _button( new UIButton( 145.0f, winHeight - 24.0f, 132.0f, 28.0f, "Test", "Interface\\Buttons\\UI-DialogBox-Button-Up.blp", "Interface\\Buttons\\UI-DialogBox-Button-Down.blp", AddM2Click, 0 ) )
 , _tbox( new UITextBox( 30.0f, 30.0f, 400.0f, 40.0f, UIDoodadSpawner__TextBoxEnter ) )
+, _treeView( UITreeView::Ptr() )
 {
+  global_doodadSpawner_evil = this;
+
   addChild( _button );
   addChild( _tbox );
+
+  Directory::Ptr fileList( new Directory() );
+
+  size_t found( std::string::npos );
+  for( std::list<std::string>::const_iterator it( gListfile.begin() ), end( gListfile.end() ); it != end; ++it )
+  {
+    if( it->find( "m2" ) != std::string::npos )
+    {
+      found = it->find_last_of("/\\");
+      if( found != std::string::npos )
+        fileList->addDirectory( it->substr( 0, found ) )->addFile( it->substr( found + 1 ) );
+      else
+        fileList->addFile( *it );
+    }
+  }
+
+  _treeView = UITreeView::Ptr( new UITreeView( 30.0f, 80.0f, "Models", fileList, UITreeView::Ptr(), UIDoodadSpawner__TreeSelect ) );
+  addChild( _treeView.get() );
 }
 
 void UIDoodadSpawner::AddM2( const std::string& filename )
