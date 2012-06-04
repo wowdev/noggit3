43489310c5aeb14ceaa9a353478c5d32192b4382
Bernd Lörwald <bloerwald@googlemail.com>
2012-06-04 23:32:13 +0200

    Fix d56f71d74a09: Formatting, some small improvements.

--- src/helper/qt/case_insensitive.h
+++ src/helper/qt/case_insensitive.h
@@ -1,3 +1,7 @@
+// non_recursive_filter_model.cpp is part of Noggit3, licensed via GNU General Publiicense (version 3).
+// Bernd Lörwald <bloerwald+noggit@googlemail.com>
+// Benedikt Kleiner <benedikt.kleiner@googlemail.com>
+
 #include <QString>
 #include <QDir>
 #include <QFile>
@@ -8,28 +12,46 @@ namespace helper
   {
     namespace case_insensitive
     {
-
       namespace detail
       {
-        QString case_sensitive_equivalent (const QString& path);
+        QString case_sensitive_equivalent (const QString& path)
+        {
+          const int pos (path.lastIndexOf (QRegExp("[\\\\/]")));
+
+          directory dir (ps < 0 ? QDir::rootPath() : path.left (pos));
+          const QStringList files (dir.entryList());
+          return dir.absoluteFilePath
+            (files.at ( files.indexOf ( QRegExp ( path.mid (pos+1)
+                                                , Qt::CaseInsensitive
+                                                )
+                                      )
+                      )
+            );
+        }
       }
 
       class directory : public QDir
       {
       public:
         directory (const QDir & dir)
-          : QDir (dir)  
+          : QDir (dir)
         { }
-        directory (const QString& path = QString())  
+        directory (const QString& path = QString())
           : QDir (detail::case_sensitive_equivalent (path))
         { }
         directory ( const QString& path
                   , const QString& nameFilter
-                  , QDir::SortFlags sort = QDir::SortFlags( QDir::Name | QDir::IgnoreCase)
+                  , QDir::SortFlags sort = QDir::SortFlags ( QDir::Name
+                                                           | QDir::IgnoreCase
+                                                           )
                   , QDir::Filters filters = QDir::AllEntries
                   )
-          : QDir (detail::case_sensitive_equivalent (path), nameFilter, sort, filters & ~QDir::CaseSensitive)
-        {}
+          : QDir ( detail::case_sensitive_equivalent (path)
+                 , nameFilter
+                 , sort
+                 , filters & ~QDir::CaseSensitive
+                 )
+        { }
 
         bool exists (const QString& name) const
         {
@@ -39,37 +61,28 @@ namespace helper
           }
 
           QDir parent (*this);
-          QStringList dirNames = name.split(QRegExp("[\\\\/]"));
+          const QStringList dirNames (name.split (QRegExp("[\\\\/]")));
 
-          foreach(QString dir, dirNames)
+          foreach (QString dir, dirNames)
           {
-              QStringList entrys(parent.entryList());
-              if(entrys.contains (dir, Qt::CaseInsensitive))
+              const QStringList entries (parent.entryList());
+              if (!entries.contains (dir, Qt::CaseInsensitive))
               {
-                  parent.cd(entrys.at(entrys.indexOf(QRegExp(dir, Qt::CaseInsensitive))));
-                  continue;
+                return false;
               }
-              return false;
+
+              parent.cd
+                ( entrys.at ( entrys.indexOf ( QRegExp ( dir
+                                                       , Qt::CaseInsensitive
+                                                       )
+                                             )
+                            )
+                );
           }
 
           return true;
         }
       };
-
-      namespace detail
-      {
-        QString case_sensitive_equivalent (const QString& path)
-        {
-          const int pos (path.lastIndexOf (QRegExp("[\\\\/]")));
-
-          if(pos < 0)
-              return QDir::rootPath();
-
-          directory dir (path.left (pos));
-          const QStringList files (dir.entryList());
-          return dir.absoluteFilePath (files.at (files.indexOf (QRegExp(path.mid (pos+1), Qt::CaseInsensitive))));
-        }
-      }
 
       class file : public QFile
       {
@@ -78,11 +91,11 @@ namespace helper
           : QFile (detail::case_sensitive_equivalent (name))
         { }
 
-        file ( QObject * parent )
+        file (QObject* parent)
           : QFile (parent)
         { }
 
-        file ( const QString & name, QObject * parent )
+        file (const QString& name, QObject* parent)
           : QFile (detail::case_sensitive_equivalent (name), parent)
         { }
       };
