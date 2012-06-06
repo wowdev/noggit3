46bf28695e20f308a27373ccd73991d93afaaec4
hanfer <benedikt.kleiner@gmail.com>
2012-06-06 13:56:44 +0200

    inital for WorldController

---
+++ src/noggit/WorldController.h
@@ -0,0 +1,30 @@
+// WorldController.h is part of Noggit3, licensed via GNU General Publiicense (version 3).
+// Bernd Lörwald <bloerwald+noggit@googlemail.com>
+// Benedikt Kleiner <benedikt.kleiner@googlemail.com>
+
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <QObject>
+#include <QString>
+
+class World;
+class MapView;
+
+class WorldController : public QObject
+{
+    Q_OBJECT
+public:
+  WorldController();
+
+  void setModel(World *world);
+  void addView(MapView *view);
+
+private:
+  QList< MapView* > _views;
+  World *_model;
+
+};
+
+
+#endif
