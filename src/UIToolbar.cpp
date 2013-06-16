b3537ed0592b522e0c832a3d067fdc881315a736
Steff <project.modcraft@googlemail.com>
2013-06-17 01:19:29 +0200

    - Implement 2 new icons for edit mode - 7 Water editor - 8 Light editor - Hotkey and direct selection works.

--- src/UIToolbar.cpp
+++ src/UIToolbar.cpp
@@ -14,7 +14,7 @@
 #include "UIToolbarIcon.h"
 
 UIToolbar::UIToolbar(float xPos, float yPos, UIMapViewGUI *setGui)
-: UIWindow( xPos, yPos, 105.0f, 280.0f, "interface\\tooltips\\ui-tooltip-border.blp" )
+: UIWindow( xPos, yPos, 105.0f, 330.0f, "interface\\tooltips\\ui-tooltip-border.blp" )
 , mainGui( setGui )
 , text( new UIText( 8, 7, "Raise/Lower", arialn13, eJustifyLeft ) )
 , selectedIcon( -1 )
@@ -24,21 +24,27 @@ UIToolbar::UIToolbar(float xPos, float yPos, UIMapViewGUI *setGui)
   addChild( text );
   addChild( new UIMinimizeButton( width() ) );
 
-  // ground edit
+  // Ground edit
   SetIcon( 0, "Interface\\ICONS\\INV_Elemental_Mote_Earth01.blp" );
   // Flat/blur
   SetIcon( 1, "Interface\\ICONS\\INV_Elemental_Mote_Air01.blp" );
-  //3D Paint
+  // 3D Paint
   SetIcon( 2, "Interface\\ICONS\\INV_Feather_16.blp" );
-  //Holes
+  // Holes
   SetIcon( 3, "Interface\\ICONS\\INV_Gizmo_HardenedAdamantiteTube.blp" );
+  // AreaID Editor
   SetIcon( 4, "Interface\\ICONS\\INV_Misc_Map07.blp" );
+  // Impassible
   SetIcon( 5, "Interface\\ICONS\\INV_Misc_Net_01.blp" );
+  // Water editor
+  SetIcon( 6, "Interface\\ICONS\\INV_Elemental_Primal_Water.blp" );
+  // Light editor
+  SetIcon( 7, "Interface\\ICONS\\INV_Enchant_ShardBrilliantSmall.blp" );
   
+  
+    IconSelect( 0 );
 
-  IconSelect( 0 );
-
-  UIWindow* texture_border = new UIWindow( 5, 180, 95.0f, 95.0f );
+  UIWindow* texture_border = new UIWindow( 5, 230, 95.0f, 95.0f );
   texture_border->addChild( current_texture );
   addChild( texture_border );
 }
@@ -55,16 +61,16 @@ extern int terrainMode;
 
 void UIToolbar::IconSelect( int pIcon )
 {
-  change_settings_window( selectedIcon, pIcon + 1 > 6 ? 0 : pIcon + 1);
+  change_settings_window( selectedIcon, pIcon + 1 > 7 ? 0 : pIcon + 1);
 
-  const char * Names[] = { "Raise / Lower", "Flatten / Blur", "3D Paint", "Holes", "AreaID Paint", "Impassible Flag" };
+  const char * Names[] = { "Raise / Lower", "Flatten / Blur", "3D Paint", "Holes", "AreaID Paint", "Impassible Flag", "Water edit", "Light edit" };
   text->setText( Names[pIcon] );
 
   terrainMode = pIcon;
 
   Environment::getInstance()->view_holelines = ( pIcon == 3 );
 
-  for( int j = 0; j < 6; j++ )
+  for( int j = 0; j < 8; j++ )
     if( mToolbarIcons[j] )
       mToolbarIcons[j]->selected = false;
 
