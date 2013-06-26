cd8f5a6bb770a00dcd8371bc7bce1d8508e0f2e1
hanfer <benedikt.kleiner@gmail.com>
2013-06-26 18:04:48 +0200

    minor is a reservered keyword on linux

--- src/MapTile.h
+++ src/MapTile.h
@@ -19,7 +19,7 @@ private:
   MH2O_Header mWaterHeaders[256];
   MH2O_Information mWaterInfos[256];
   char* MH2O_Buffer; // Water info storage...
-  std::map<int, std::vector<unsigned char>> mWaterMasks;
+  std::map<int, std::vector<unsigned char> > mWaterMasks;
 public:
   MapTile( int x0, int z0, const std::string& pFilename, bool pBigAlpha );
   ~MapTile();
