289f68ed2ccedbb0969724622c2670b69ce65052
hanfer <benedikt.kleiner@gmail.com>
2013-10-20 20:02:43 +0200

    include icon in exe

--- cmake/win32_postfind.cmake
+++ cmake/win32_postfind.cmake
@@ -4,13 +4,20 @@ LINK_DIRECTORIES(${Boost_STRIPPED_LIB_DIR} ${STORMLIB_LIBRARY_DIR})
 #maybe in wrong order now
 SET(IncludeDirectories ${IncludeDirectories} "${CMAKE_SOURCE_DIR}/include/win/")
 SET(SourceFiles ${SourceFiles} "${CMAKE_SOURCE_DIR}/include/win/StackWalker.cpp")
-ADD_DEFINITIONS(/vmg /D NOMINMAX)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /vmg /D NOMINMAX")
 
+# warnings?
+if( NOGGIT_ALL_WARNINGS )
+  	MESSAGE( STATUS "Spilling out mass warnings." )
+  	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4 /Wall /Wp64")
+else(NOGGIT_ALL_WARNINGS)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W1 /Wall /Wp64")
+endif( NOGGIT_ALL_WARNINGS )
 
-# Set loglevel to w1 on windows and activate
-add_definitions(/W1) 
 # mark 32 bit executables large address aware so they can use > 2GB address space
 if(CMAKE_SIZEOF_VOID_P MATCHES 4)
 	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /LARGEADDRESSAWARE")
 	message(STATUS "- MSVC: Enabled large address awareness!")
 endif()
+
+set(ResFiles media/res.rc)
