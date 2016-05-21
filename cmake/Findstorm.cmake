07a8d30d9ef52b10b270f5c7539daf0966cf30a7
Bernd L?rwald <bloerwald@googlemail.com>
2016-05-21 16:13:18 +0200

    cmake: findStorm: rewrite to support non-windows again, but hopefully still support windows

--- cmake/Findstorm.cmake
+++ cmake/Findstorm.cmake
@@ -1,37 +1,19 @@
 # Findstorm.cmake is part of Noggit3, licensed via GNU General Public License (version 3).
-# Bernd Lörwald <bloerwald+noggit@googlemail.com>
 
-#find STORM
-# STORM_LIBRARIES, the name of the library to link against
-# STORM_FOUND, if false, do not try to link
-# STORM_INCLUDES,
+find_path (STORM_INCLUDE_DIR StormLib.h StormPort.h)
 
-find_path(STORM_INCLUDE_DIR StormLib.h StormPort.h)
-#find_path(STORM_LIBRARY_DIR StormLibRAD.lib)
+find_library (_storm_debug_lib NAMES StormLibDAD StormLibDAS StormLibDUD StormLibDUS)
+find_library (_storm_release_lib NAMES StormLibRAD StormLibRAS StormLibRUD StormLibRUS)
+find_library (_storm_any_lib NAMES storm stormlib StormLib)
 
-SET(CMAKE_FIND_LIBRARY_PREFIXES "")
-SET(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll")
+set (STORM_LIBRARIES)
+if (_storm_debug_lib AND _storm_release_lib)
+  list (APPEND STORM_LIBRARIES debug ${_storm_debug_lib} optimized ${_storm_release_lib})
+else()
+  list (APPEND STORM_LIBRARIES ${_storm_any_lib})
+endif()
 
-find_library(STORM_LIBRARY StormLibRAD.lib)
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args (storm DEFAULT_MSG STORM_LIBRARIES STORM_INCLUDE_DIR)
 
-if( WIN32 )
-  find_library(STORM_LIBRARYRAD StormLibRAD.lib)
-  find_library(STORM_LIBRARYDAD StormLibDAD.lib)
-  set(STORM_LIBRARIES ${STORM_LIBRARYRAD} ${STORM_LIBRARYDAD})
-else( WIN32 )
-set(STORM_LIBRARIES ${STORM_LIBRARY})
-endif( WIN32 )
-
-
-
-
-if(STORM_INCLUDE_DIR AND STORM_LIBRARY)
-    SET(STORM_FOUND TRUE)
-endif(STORM_INCLUDE_DIR AND STORM_LIBRARY)
-
-set(STORM_INCLUDES ${STORM_INCLUDE_DIR})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(storm DEFAULT_MSG STORM_LIBRARYRAD STORM_INCLUDE_DIR)
-
-mark_as_advanced(STORM_INCLUDE_DIR STORM_LIBRARYRAD STORM_LIBRARYDAD)
+mark_as_advanced (STORM_INCLUDE_DIR _storm_debug_lib _storm_release_lib _storm_any_lib STORM_LIBRARIES)
