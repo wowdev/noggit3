239c9b32e5167d8cac0b35c61dc03b0139eb4e96
Kaev <Demonhunter@hotmail.de>
2016-05-26 22:07:01 +0200

    Fixed a bug regarding the model bug fix and added me to the title :P

--- src/UIAbout.cpp
+++ src/UIAbout.cpp
@@ -17,8 +17,8 @@ UIAbout::UIAbout()
 	addChild(new UIText(73.0f, 24.0f, "Noggit Studio", app.getSkurri32(), eJustifyLeft));
 	addChild(new UIText(155.0f, 57.0f, "a wow map editor for 3.3.5a", app.getFritz16(), eJustifyLeft));
 	addChild(new UIText(20.0f, 95.0f, "Ufoz [...],   Cryect,   Beket,   Schlumpf,   Tigurius", app.getFritz16(), eJustifyLeft));
-	addChild(new UIText(25.0f, 115.0f, " Steff,  Garthog,  Glararan,  Cromon,  Hanfer", app.getFritz16(), eJustifyLeft));
-	addChild(new UIText(100.0f, 135.0f, " Skarn,  AxelSheva  &  Valium", app.getFritz16(), eJustifyLeft));
+	addChild(new UIText(20.0f, 115.0f, "Steff,  Garthog,  Glararan,  Cromon,  Hanfer", app.getFritz16(), eJustifyLeft));
+	addChild(new UIText(20.0f, 135.0f, "Skarn,  AxelSheva,  Valium,  Kaev", app.getFritz16(), eJustifyLeft));
 	addChild(new UIText(20.0f, 165.0f, "World of Warcraft is (C) Blizzard Entertainment", app.getFritz16(), eJustifyLeft));
 	addChild(new UIText(20.0f, 190.0f, STRPRODUCTVER, app.getFritz16(), eJustifyLeft));
 	addChild(new UIText(375.0f, 190.0f, __DATE__ ", " __TIME__, app.getFritz16(), eJustifyRight));
