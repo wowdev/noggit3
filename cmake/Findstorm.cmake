2046eb5356a7e081327959fe7eb986e4ff9ee3cb
Bernd Lörwald <bloerwald@googlemail.com>
2016-06-11 13:22:24 +0200

    cmake: use interface libraries for boost and storm
    * also disable autolinking of storm so that cmake actually defines what libraries of storm are used

--- cmake/Findstorm.cmake
+++ cmake/Findstorm.cmake
@@ -17,3 +17,11 @@ include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (storm DEFAULT_MSG STORM_LIBRARIES STORM_INCLUDE_DIR)
 
 mark_as_advanced (STORM_INCLUDE_DIR _storm_debug_lib _storm_release_lib _storm_any_lib STORM_LIBRARIES)
+
+add_library (StormLib INTERFACE)
+target_link_libraries (StormLib INTERFACE ${STORM_LIBRARIES})
+set_property  (TARGET StormLib APPEND PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${STORM_INCLUDE_DIR})
+set_property  (TARGET StormLib APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${STORM_INCLUDE_DIR})
+
+#! \note on Windows, storm tries to auto-link. There is no proper flag to disable that, so abuse this one.
+target_compile_definitions (StormLib INTERFACE -D__STORMLIB_SELF__)
