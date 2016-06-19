9cc9e71642e2946f926ae400a56baa85e32e22c8
Bernd Lörwald <bloerwald@googlemail.com>
2016-06-19 17:37:19 +0200

    hg: try unbreaking ray.hpp

--- src/math/ray.hpp
+++ src/math/ray.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
 #include <boost/optional/optional.hpp>
-#include <math/vector_3d.h>
-namespace math{
-  struct ray  {    vector_3d origin;    vector_3d direction;  };
-  boost::optional<float> intersect_bounds(ray const& _ray, vector_3d const& _min, vector_3d const& _max);  boost::optional<float> intersect_triangle(ray const& _ray, vector_3d const& _v0, vector_3d const& _v1, vector_3d const& _v2);}
 
+#include <math/vector_3d.h>
+
+namespace math
+{
+  struct ray
+  {
+    vector_3d origin;
+    vector_3d direction;
+  };
+  boost::optional<float> intersect_bounds(ray const& _ray, vector_3d const& _min, vector_3d const& _max);
+  boost::optional<float> intersect_triangle(ray const& _ray, vector_3d const& _v0, vector_3d const& _v1, vector_3d const& _v2);
+}
