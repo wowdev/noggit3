b419d817b96318a6ae6a0bdcb3e6b3541a352c55
Adspartan <adspartan04@gmail.com>
2016-08-12 17:39:09 +0200

    add filter for model name in the model selector

--- src/UIModelImport.h
+++ src/UIModelImport.h
@@ -2,16 +2,19 @@
 #define __MODEL_IMPORT_H
 
 #include "UICloseWindow.h"
+#include "UITextBox.h"
 #include "MapView.h"
+
 class UIListView;
 
 class UIModelImport : public UICloseWindow
 {
 private:
 	static const int winWidth = 440;
-	static const int winHeight = 240;
+	static const int winHeight = 270;
 	MapView *_mapView;
 	UIListView* MoldelList;
+  UITextBox* _textBox;
 
 public:
 	UIModelImport(MapView *mapview);
