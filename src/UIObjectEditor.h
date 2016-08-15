7d72fa4634249ff9e7d874fa2a02803fa1863425
Adspartan <adspartan04@gmail.com>
2016-08-15 14:54:24 +0200

    move checkbox for model pasting from edit menu to object editor show the name of the model to be pasted in the object editor

--- src/UIObjectEditor.h
+++ src/UIObjectEditor.h
@@ -2,20 +2,28 @@
 #define __UIOBJECTEDITOR_H
 
 #include "UIWindow.h"
-#include "UICheckBox.h"
 #include "Selection.h"
 #include "Vec3D.h"
+
+class UIModelImport;
+class UIStatusBar;
+class UIMapViewGUI;
 
 class UIObjectEditor : public UIWindow
 {
 public:
-  UIObjectEditor(float x, float y);
+  UIObjectEditor(float x, float y, UIMapViewGUI* mainGui);
 
   void copy(nameEntry entry);
   void pasteObject(Vec3D pos);
 
+  UIModelImport *modelImport;
+  UIStatusBar *filename;
+
 private:
   nameEntry selected;
+
+  void setModelName(const std::string &name);
 };
 
 
