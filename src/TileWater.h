ce4e72398fc863e713fb1ba333ff43c3391c407c
Adspartan <adspartan04@gmail.com>
2017-01-06 23:28:19 +0100

    TileWater: fix x/z coords

--- src/TileWater.h
+++ src/TileWater.h
@@ -15,22 +15,22 @@ public:
 	TileWater(MapTile *pTile, float pXbase, float pZbase);
 	~TileWater(void);
 
-	ChunkWater* getChunk(int x, int y);
+	ChunkWater* getChunk(int x, int z);
 
 	void readFromFile(MPQFile &theFile, size_t basePos);
 	void saveToFile(sExtendableArray &lADTFile, int &lMHDR_Position, int &lCurrentPosition);
 
 	void draw();
   bool hasData(size_t layer);
-	float HaveWater(int i, int j);
-	void CropMiniChunk(int i, int j, MapChunk* chunkTerrain);
+	float HaveWater(int x, int z);
+	void CropMiniChunk(int x, int z, MapChunk* chunkTerrain);
 
 	void autoGen(int factor);
 
 	void setHeight(float height, size_t layer);
-	void setHeight(int i, int j, float height, size_t layer);
+	void setHeight(int x, int z, float height, size_t layer);
 	float getHeight(size_t layer);
-	float getHeightChunk(int i, int j, size_t layer);
+	float getHeightChunk(int x, int z, size_t layer);
 
 	void setTrans(unsigned char opacity, size_t layer);
 	unsigned char getOpacity(size_t layer);
@@ -39,12 +39,12 @@ public:
 	int getType(size_t layer);
 
 	void addLayer(size_t layer);
-	void addLayer(int i, int j, size_t layer);
+	void addLayer(int x, int z, size_t layer);
 	void addLayer(float height, unsigned char trans, size_t layer);
-	void addLayer(int i, int j, float height, unsigned char trans, size_t layer);
+	void addLayer(int x, int z, float height, unsigned char trans, size_t layer);
 
 	void deleteLayer(size_t layer);
-	void deleteLayer(int i, int j, size_t layer);
+	void deleteLayer(int x, int z, size_t layer);
 
 private:
 	void reload();
