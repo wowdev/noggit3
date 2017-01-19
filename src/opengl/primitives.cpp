e2624fdaf66ae47a4a543bf0bf71db2eb11518f6
Bernd Lörwald <bloerwald@googlemail.com>
2017-01-19 17:08:23 +0100

    move DrawABox to opengl/primitives

---
+++ src/opengl/primitives.cpp
@@ -0,0 +1,61 @@
+// This file is part of Noggit3, licensed under GNU General Public License (version 3).
+
+#include <opengl/primitives.hpp>
+
+#include <math/vector_4d.hpp>
+
+#include <opengl/context.hpp>
+#include <opengl/types.hpp>
+
+namespace opengl
+{
+  namespace primitives
+  {
+    wire_box::wire_box ( const ::math::vector_3d& min_point
+             , const ::math::vector_3d& max_point
+             )
+      : _min_point (min_point)
+      , _max_point (max_point)
+    { }
+
+    void wire_box::draw ( const ::math::vector_4d& color
+                        , const float& line_width
+                        ) const
+    {
+      gl.enable (GL_LINE_SMOOTH);
+      gl.hint (GL_LINE_SMOOTH_HINT, GL_NICEST);
+      gl.lineWidth (line_width);
+
+      gl.color4fv (color);
+
+      gl.begin (GL_LINE_STRIP);
+      gl.vertex3f (_min_point.x, _max_point.y, _min_point.z);
+      gl.vertex3f (_min_point.x, _min_point.y, _min_point.z);
+      gl.vertex3f (_max_point.x, _min_point.y, _min_point.z);
+      gl.vertex3f (_max_point.x, _min_point.y, _max_point.z);
+      gl.vertex3f (_max_point.x, _max_point.y, _max_point.z);
+      gl.vertex3f (_max_point.x, _max_point.y, _min_point.z);
+      gl.vertex3f (_min_point.x, _max_point.y, _min_point.z);
+      gl.vertex3f (_min_point.x, _max_point.y, _max_point.z);
+      gl.vertex3f (_min_point.x, _min_point.y, _max_point.z);
+      gl.vertex3f (_min_point.x, _min_point.y, _min_point.z);
+      gl.end();
+
+      gl.begin (GL_LINES);
+      gl.vertex3f (_min_point.x, _min_point.y, _max_point.z);
+      gl.vertex3f (_max_point.x, _min_point.y, _max_point.z);
+      gl.end();
+
+      gl.begin (GL_LINES);
+      gl.vertex3f (_max_point.x, _max_point.y, _min_point.z);
+      gl.vertex3f (_max_point.x, _min_point.y, _min_point.z);
+      gl.end();
+
+      gl.begin (GL_LINES);
+      gl.vertex3f (_min_point.x, _max_point.y, _max_point.z);
+      gl.vertex3f (_max_point.x, _max_point.y, _max_point.z);
+      gl.end();
+
+    }
+  }
+}
---
+++ src/opengl/primitives.hpp
@@ -0,0 +1,30 @@
+// This file is part of Noggit3, licensed under GNU General Public License (version 3).
+
+#pragma once
+
+#include <math/vector_3d.hpp>
+
+namespace math
+{
+  class vector_4d;
+}
+
+namespace opengl
+{
+  namespace primitives
+  {
+    class wire_box
+    {
+    public:
+      wire_box ( const ::math::vector_3d& min_point
+               , const ::math::vector_3d& max_point
+               );
+
+      void draw (const ::math::vector_4d& color, const float& line_width) const;
+
+    private:
+      const ::math::vector_3d _min_point;
+      const ::math::vector_3d _max_point;
+    };
+  }
+}
