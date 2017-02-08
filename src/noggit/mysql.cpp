8cfa791e6191384f1f6479a7778c8050e67d1c4c
Bernd L?rwald <bloerwald@googlemail.com>
2017-02-08 10:47:05 +0100

    mysql: pass settings object in one more layer

--- src/noggit/mysql.cpp
+++ src/noggit/mysql.cpp
@@ -6,23 +6,21 @@
 #include <cppconn/driver.h>
 #include <cppconn/prepared_statement.h>
 
-std::unique_ptr<sql::Connection> connect()
+namespace
 {
-  const char* server = Settings::getInstance()->mysql->Server.c_str();
-  const char* user = Settings::getInstance()->mysql->User.c_str();
-  const char* password = Settings::getInstance()->mysql->Pass.c_str();
-  const char* database = Settings::getInstance()->mysql->Database.c_str();
+  std::unique_ptr<sql::Connection> connect (Settings::mysql_connection_info const& info)
+  {
+    std::unique_ptr<sql::Connection> Con
+      (get_driver_instance()->connect (info.Server, info.User, info.Pass));
+    Con->setSchema(info.Database);
 
-  std::unique_ptr<sql::Connection> Con
-    (get_driver_instance()->connect (server, user, password));
-  Con->setSchema(database);
-
-  return Con;
+    return Con;
+  }
 }
 
 uint32_t Mysql::getGUIDFromDB()
 {
-  auto Con (connect());
+  auto Con (connect (*Settings::getInstance()->mysql));
   std::unique_ptr<sql::PreparedStatement> pstmt (Con->prepareStatement ("SELECT UID FROM UIDs"));
   std::unique_ptr<sql::ResultSet> res (pstmt->executeQuery());
 
@@ -37,7 +35,7 @@ uint32_t Mysql::getGUIDFromDB()
 
 void Mysql::UpdateUIDInDB(uint32_t NewUID)
 {
-  auto Con (connect());
+  auto Con (connect (*Settings::getInstance()->mysql));
   std::unique_ptr<sql::PreparedStatement> pstmt (Con->prepareStatement ("UPDATE UIDs SET UID=(?)"));
   pstmt->setInt (1, NewUID);
   pstmt->executeUpdate();
