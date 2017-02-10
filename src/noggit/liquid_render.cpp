984c8a6c53fe54cac355ec0882092017a9844dc3
Bernd L?rwald <bloerwald@googlemail.com>
2017-02-10 20:46:13 +0100

    liquid_render: let shader and index buffer be a member

--- src/noggit/liquid_render.cpp
+++ src/noggit/liquid_render.cpp
@@ -7,6 +7,7 @@
 #include <noggit/World.h>
 #include <opengl/context.hpp>
 #include <opengl/matrix.hpp>
+#include <opengl/scoped.hpp>
 
 #include <boost/filesystem.hpp>
 #include <boost/format.hpp>
@@ -16,54 +17,6 @@
 
 void liquid_render::draw (std::function<void (opengl::scoped::use_program&)> actual)
 {
-  static opengl::program const program
-    { { GL_VERTEX_SHADER
-      , R"code(
-#version 110
-
-attribute vec4 position;
-attribute vec2 tex_coord;
-attribute float depth;
-
-uniform mat4 model_view;
-uniform mat4 projection;
-
-varying float depth_;
-varying vec2 tex_coord_;
-
-void main()
-{
-  depth_ = depth;
-  tex_coord_ = tex_coord;
-
-  gl_Position = projection * model_view * position;
-}
-)code"
-      }
-    , { GL_FRAGMENT_SHADER
-      , R"code(
-#version 110
-
-uniform sampler2D texture;
-uniform vec4 color_light;
-uniform vec4 color_dark;
-uniform float tex_repeat;
-
-varying float depth_;
-varying vec2 tex_coord_;
-
-void main()
-{
-  vec4 texel = texture2D (texture, tex_coord_ / tex_repeat);
-  vec4 lerp = mix (color_dark, color_light, depth_);
-  vec4 tResult = clamp (texel + lerp, 0.0, 1.0); //clamp shouldn't be needed
-  vec4 oColor = clamp (texel + tResult, 0.0, 1.0);
-  gl_FragColor = vec4 (oColor.rgb, lerp.a) * 0.000001 + vec4 (1.0, 0.0, 1.0, 1.0);
-}
-)code"
-      }
-    };
-
   opengl::scoped::use_program water_shader {program};
 
   water_shader.uniform ("model_view", opengl::matrix::model_view());
--- src/noggit/liquid_render.hpp
+++ src/noggit/liquid_render.hpp
@@ -23,6 +23,55 @@ public:
   void setTransparency(bool b) { _transparency = b; }
 
 private:
+  opengl::program const program
+    { { GL_VERTEX_SHADER
+      , R"code(
+#version 110
+
+attribute vec4 position;
+attribute vec2 tex_coord;
+attribute float depth;
+
+uniform mat4 model_view;
+uniform mat4 projection;
+
+varying float depth_;
+varying vec2 tex_coord_;
+
+void main()
+{
+  depth_ = depth;
+  tex_coord_ = tex_coord;
+
+  gl_Position = projection * model_view * position;
+}
+)code"
+      }
+    , { GL_FRAGMENT_SHADER
+      , R"code(
+#version 110
+
+uniform sampler2D texture;
+uniform vec4 color_light;
+uniform vec4 color_dark;
+uniform float tex_repeat;
+
+varying float depth_;
+varying vec2 tex_coord_;
+
+void main()
+{
+  vec4 texel = texture2D (texture, tex_coord_ / tex_repeat);
+  vec4 lerp = mix (color_dark, color_light, depth_);
+  vec4 tResult = clamp (texel + lerp, 0.0, 1.0); //clamp shouldn't be needed
+  vec4 oColor = clamp (texel + tResult, 0.0, 1.0);
+  gl_FragColor = vec4 (oColor.rgb, lerp.a);
+}
+)code"
+      }
+    };
+
+
   bool _transparency;
 
   std::vector<scoped_blp_texture_reference> _textures;
