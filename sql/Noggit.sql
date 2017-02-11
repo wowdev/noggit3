cc4befbbb42fba260a79fcd1f23809b8ccde691d
Funkybeatz <michael@hubenthals-aussies.de>
2017-02-11 19:08:49 +0100

    Noggit.sql: Right order (for better overview), removing Data that is not needed

--- sql/Noggit.sql
+++ sql/Noggit.sql
@@ -1,10 +1,7 @@
 CREATE TABLE `UIDs` (
-  `UID` int(11) NOT NULL,
-  `MapId` int(11) NOT NULL
+  `MapId` int(11) NOT NULL,
+  `UID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `UIDs` (`UID`) VALUES
-(0, 0);
-
 ALTER TABLE `UIDs`
-  ADD PRIMARY KEY (`UID`);
+  ADD PRIMARY KEY (`MapId`);
