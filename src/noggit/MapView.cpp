6fdeb286885cc7b50a9825fc60f0075fa7beec50
Bernd L?rwald <bloerwald@googlemail.com>
2017-02-11 20:45:27 +0100

    pass world::lookat around a bit more

--- src/noggit/MapView.cpp
+++ src/noggit/MapView.cpp
@@ -2098,6 +2098,7 @@ void MapView::displayViewMode_3D(float /*t*/, float /*dt*/)
                , terrainMode == editing_mode::areaid
                , terrainMode
                , gWorld->camera
+               , gWorld->lookat
                );
 
   displayGUIIfEnabled();
