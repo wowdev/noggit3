c377d1f8d4fc8ef74fb6456fc412a3eef494f022
hanfer <benedikt.kleiner@gmail.com>
2017-02-12 21:44:33 +0100

    map_horizon: add drawing code for low res terrain

--- src/noggit/map_horizon.h
+++ src/noggit/map_horizon.h
@@ -2,7 +2,10 @@
 
 #pragma once
 
+#include <noggit/Frustum.h>
+
 #include <opengl/texture.hpp>
+#include <opengl/scoped.hpp>
 
 #include <memory>
 
@@ -14,12 +17,34 @@ struct map_horizon_tile
     int16_t height_16[16][16];
 };
 
+struct map_horizon_batch
+{
+  map_horizon_batch ()
+    : vertex_start (0)
+    , vertex_count (0)
+  {}
+
+  map_horizon_batch (uint32_t _vertex_start, uint32_t _vertex_count)
+    : vertex_start(_vertex_start)
+    , vertex_count(_vertex_count)
+  {}
+
+  uint32_t vertex_start;
+  uint32_t vertex_count;
+};
+
 class map_horizon
 {
 public:
     map_horizon(const std::string& basename);
 
     void upload();
+
+    void draw( MapIndex *index
+             , const math::vector_3d& color
+             , const float& cull_distance
+             , const Frustum& frustum
+             , const math::vector_3d& camera );
 
     opengl::texture minimap;
 
@@ -28,7 +53,16 @@ public:
     bool _finished_upload;
 
 private:
+    void upload_minimap();
+    void upload_horizon();
+
     std::string _filename;
+
+    map_horizon_batch _batches[64][64];
+
+    opengl::scoped::buffers<2> _buffers;
+    GLuint const& _index_buffer = _buffers[0];
+    GLuint const& _vertex_buffer = _buffers[1];
 
     std::unique_ptr<map_horizon_tile> _tiles[64][64];
 };
