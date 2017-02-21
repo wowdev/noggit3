caa6f4df3c41198a54f877250d790e7389cc112f
Bernd L?rwald <bloerwald@googlemail.com>
2017-02-21 23:25:36 +0100

    ui: uid_fix_window: qtify

--- src/noggit/ui/uid_fix_window.cpp
+++ src/noggit/ui/uid_fix_window.cpp
@@ -2,60 +2,69 @@
 
 #include <noggit/ui/uid_fix_window.hpp>
 
-#include <noggit/application.h>
-#include <noggit/ui/Button.h>
 #include <noggit/map_index.hpp>
 #include <noggit/Menu.h>
 #include <noggit/World.h>
 
-
-void fixAll(UIFrame* f, int)
-{
-  static_cast<ui::uid_fix_window*>(f->parent())->fixAllTiles();
-}
-
-void maxUID(UIFrame* f, int)
-{
-  static_cast<ui::uid_fix_window*>(f->parent())->getMaxUID();
-}
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
 
 namespace ui
 {
   uid_fix_window::uid_fix_window(Menu* menu)
-    : UIWindow((video.xres() - winWidth) / 2.0f, (video.yres() - winHeight) / 2.0f, winWidth, winHeight)
-    , _menuLink(menu)
+    : QDialog (nullptr)
+    , _menuLink (menu)
   {
-    addChild(new UIText(10.0f, 10.0f, "Required: Models UID fix (run only once)", app.getArial14(), eJustifyLeft));
+    new QVBoxLayout (this);
 
-    addChild(new UIButton(5.0f, 40.0f, 66.6f, 35.0f, "Fix All", "Interface\\BUTTONS\\UI-DialogBox-Button-Disabled.blp", "Interface\\BUTTONS\\UI-DialogBox-Button-Down.blp", fixAll, 0));
-    addChild(new UIText(80.0f, 40.0f, "remove duplicates / fix collisions", app.getArial14(), eJustifyLeft));
-    addChild(new UIText(10.0f, 65.0f, "Warning /!\\", app.getArial14(), eJustifyLeft));
-    addChild(new UIText(10.0f, 85.0f, "recommended for custom maps, edit all adts", app.getArial14(), eJustifyLeft));
+    layout()->addWidget
+      ( new QLabel ( "In order to avoid issues with duplicating or missing\n"
+                     "models, it is required to either determine the maximum\n"
+                     "unique ID of all objects on a map, or to fix do that and\n"
+                     "also fix all existing models on the map.\n"
+                     "\n"
+                     "For existing maps that have been previously edited it is\n"
+                     "recommended to fix up, while for untouched, Blizzard maps\n"
+                     "it is sufficient to only determine the currently highest\n"
+                     "unique ID. Getting the currently maximum unique ID will\n"
+                     "only prevent new issues, while fixing all takes a few\n"
+                     "seconds and will also remove existing issues.\n"
+                     "\n"
+                     "This is only required once."
+                   , this
+                   )
+      );
 
-    addChild(new UIText(5.0f, 110.0f, "Or", app.getArial14(), eJustifyLeft));
+    auto buttons (new QDialogButtonBox (this));
+    auto fix_all (buttons->addButton ("Fix All", QDialogButtonBox::AcceptRole));
+    auto get_max (buttons->addButton ("Get Max UID", QDialogButtonBox::YesRole));
 
-    addChild(new UIButton(5.0f, 135.0f, 100.0f, 35.0f, "Get Max UID", "Interface\\BUTTONS\\UI-DialogBox-Button-Disabled.blp", "Interface\\BUTTONS\\UI-DialogBox-Button-Down.blp", maxUID, 0));
-    addChild(new UIText(120.0f, 135.0f, "only prevent new UID bug", app.getArial14(), eJustifyLeft));
-    addChild(new UIText(10.0f, 160.0f, "recommended for blizzard's maps", app.getArial14(), eJustifyLeft));
+    connect ( fix_all, &QPushButton::clicked
+            , [this]
+              {
+                hide();
+                gWorld->mapIndex.fixUIDs();
+                _menuLink->enterMapAt (_pos);
+              }
+            );
+
+    connect ( get_max, &QPushButton::clicked
+            , [this]
+              {
+                hide();
+                gWorld->mapIndex.searchMaxUID();
+                _menuLink->enterMapAt (_pos);
+              }
+            );
+
+    layout()->addWidget (buttons);
   }
 
   void uid_fix_window::enterAt(math::vector_3d const& pos)
   {
     _pos = pos;
     show();
-  }
-
-  void uid_fix_window::fixAllTiles()
-  {
-    hide();
-    gWorld->mapIndex.fixUIDs();
-    _menuLink->enterMapAt(_pos);
-  }
-
-  void uid_fix_window::getMaxUID()
-  {
-    hide();
-    gWorld->mapIndex.searchMaxUID();
-    _menuLink->enterMapAt(_pos);
   }
 }
