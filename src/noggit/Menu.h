64ca42c96b5e0484268956609603c0f6af90ee9d
Bernd Lörwald <bloerwald@googlemail.com>
2017-02-23 21:07:36 +0100

    move menu to ui::main_window and leave empty appstate behind

--- src/noggit/Menu.h
+++ src/noggit/Menu.h
@@ -2,63 +2,10 @@
 
 #pragma once
 
-#include <math/vector_3d.hpp>
 #include <noggit/AppState.h>
-
-#include <boost/optional.hpp>
-
-#include <memory>
-#include <string>
-#include <vector>
-
-// ui classes
-class UIFrame;
-class UIStatusBar;
-class UIAbout;
-class UISettings;
-namespace ui
-{
-  class uid_fix_window;
-}
-
-
-class World;
-class MapView;
-
-struct MapEntry
-{
-  int mapID;
-  std::string name;
-  int areaType;
-};
-
-struct BookmarkEntry
-{
-  int mapID;
-  std::string name;
-  math::vector_3d pos;
-  float ah;
-  float av;
-};
 
 class Menu : public AppState
 {
 public:
-  Menu();
-  ~Menu();
-
   void display(float t, float dt);
-
-  //! \todo Make private when new buttons are implemented.
-  void loadMap(int mapID);
-
-  //! \brief Enter the the map on the given location.
-  void enterMapAt(math::vector_3d pos, float av = -30.0f, float ah = -90.0f);
-
-private:
-  std::vector<MapEntry> mMaps;
-  std::vector<BookmarkEntry> mBookmarks;
-
-  void createBookmarkList();
-  void buildMenuBar();
 };
