4141ebb9d7ebad957a0c5509b7fe5e5867c4a755
Adspartan <adspartan04@gmail.com>
2017-02-26 20:46:29 +0100

    rename TextureSwitcher into texture_swapper texture_swapper: use qt

--- src/noggit/ui/TextureSwitcher.cpp
+++ src/noggit/ui/TextureSwitcher.cpp
@@ -1,46 +1,56 @@
 // This file is part of Noggit3, licensed under GNU General Public License (version 3).
 
-#include <noggit/World.h>
-#include <noggit/ui/Button.h>
-#include <noggit/ui/Texture.h>
-#include <noggit/ui/TextureSwitcher.h>
+#include <noggit/ui/texture_swapper.hpp>
+
+#include <math/vector_3d.hpp>
 #include <noggit/ui/TexturingGUI.h>
+#include <noggit/World.h>
 
-UITextureSwitcher::UITextureSwitcher (float x_right, float y, UIWindow* parent, const math::vector_3d* camera_pos)
-  : UICloseWindow (x_right - 130, y, 130, 200, "swapper", true, [parent] { parent->show(); })
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+
+UItexture_swapper::UItexture_swapper (QWidget* parent, const math::vector_3d* camera_pos)
+  : QWidget (parent)
+  , _texture_to_swap()
 {
-  float const textureSize = 110.f;
+  setWindowTitle ("Swap");
+  setWindowFlags (Qt::Tool | Qt::WindowStaysOnTopHint);
 
-  addChild (_textureFrom = new UITexture(10, 60, textureSize, textureSize, "tileset\\generic\\black.blp"));
+  auto layout (new QFormLayout (this));
 
-  addChild ( new UIButton ( 10
-                          , 65 + textureSize
-                          , textureSize
-                          , 30
-                          , "set destination"
-                          , "Interface\\BUTTONS\\UI-DialogBox-Button-Disabled.blp"
-                          , "Interface\\BUTTONS\\UI-DialogBox-Button-Down.blp"
-                          , [this]
-                            {
-                              if (!!UITexturingGUI::getSelectedTexture())
-                              {
-                                _textureFrom->setTexture (*UITexturingGUI::getSelectedTexture());
-                              }
-                            }
-                          )
-           );
+  QLabel* texture_display = new QLabel (this);
+  texture_display->setMinimumSize(64, 64);
+  texture_display->setText("PH: fix it schlumpf !");
 
-  addChild ( new UIButton ( 10
-                          , 30
-                          , textureSize
-                          , 30
-                          , "swap ADT"
-                          , "Interface\\BUTTONS\\UI-DialogBox-Button-Disabled.blp"
-                          , "Interface\\BUTTONS\\UI-DialogBox-Button-Down.blp"
-                          , [this, camera_pos]
-                            {
-                              gWorld->swapTexture (*camera_pos, current_texture());
-                            }
-                          )
-           );
+  QPushButton* select = new QPushButton("Select", this);
+  QPushButton* swap_adt = new QPushButton("Swap ADT", this);
+
+  layout->addRow(texture_display);
+  layout->addRow(select);
+  layout->addRow(swap_adt);
+
+  connect(select, &QPushButton::clicked, [this, texture_display]() {
+    _texture_to_swap = *UITexturingGUI::getSelectedTexture();
+    if (_texture_to_swap)
+    {
+      texture_display->setPixmap (noggit::render_blp_to_pixmap (_texture_to_swap.get()->filename().c_str()));
+    }
+  });
+
+  connect(swap_adt, &QPushButton::clicked, [this, camera_pos]() {
+    if (_texture_to_swap)
+    {
+      gWorld->swapTexture (*camera_pos, _texture_to_swap.get());
+    }
+  });
+}
+
+void UItexture_swapper::closeEvent(QCloseEvent *event)
+{
+  auto parent (parentWidget());
+  if (parent)
+  {
+    parent->show();
+  }
 }
