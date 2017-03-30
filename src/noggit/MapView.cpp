eebaaf77617796763a16697c306f38551052a93d
Adspartan <adspartan04@gmail.com>
2017-03-30 12:56:36 +0200

    mapview: reduce delta_for_range return value

--- src/noggit/MapView.cpp
+++ src/noggit/MapView.cpp
@@ -2746,7 +2746,7 @@ void MapView::wheelEvent (QWheelEvent* event)
     ( [&] (float range)
       {
         //! \note / 8.f for degrees, / 40.f for smoothness
-        return (_mod_ctrl_down ? 0.1f : 1.f) * range * event->angleDelta().y() / 320.f;
+        return (_mod_ctrl_down ? 0.01f : 0.1f) * range * event->angleDelta().y() / 320.f;
       }
     );
 
