bfa094099273b1852dcfc010c5651c35fb95470d
Adspartan <adspartan04@gmail.com>
2017-07-09 20:06:53 +0200

    uid_storage: fix

--- src/noggit/uid_storage.cpp
+++ src/noggit/uid_storage.cpp
@@ -10,17 +10,17 @@
 bool uid_storage::hasMaxUIDStored(uint32_t mapID)
 {
   QSettings settings;
-  return settings.value ("project/uids/" + mapID, -1).toUInt () != -1;
+  return settings.value ("project/uids/" + QString(mapID), -1).toUInt () != -1;
 }
 
 uint32_t uid_storage::getMaxUID(uint32_t mapID)
 {
   QSettings settings;
-  return settings.value ("project/uids/" + mapID, 0).toUInt();
+  return settings.value ("project/uids/" + QString(mapID), 0).toUInt();
 }
 
 void uid_storage::saveMaxUID(uint32_t mapID, uint32_t uid)
 {
   QSettings settings;
-  settings.setValue ("project/uids/" + mapID, uid);
+  settings.setValue ("project/uids/" + QString(mapID), uid);
 }
