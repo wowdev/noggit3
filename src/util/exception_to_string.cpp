8806ffd7e4ebc28dbd429b5eb85e39a4a0cfee91
Bernd Lörwald <bloerwald@googlemail.com>
2017-12-25 19:38:36 +0100

    application: catch and print and messagebox exceptions

---
+++ src/util/exception_to_string.cpp
@@ -0,0 +1,47 @@
+// This file is part of Noggit3, licensed under GNU General Public License (version 3).
+
+#include <util/exception_to_string.hpp>
+
+namespace util
+{
+  namespace
+  {
+    std::string exception_to_string_impl
+      (std::exception_ptr const& e, std::size_t indentation, bool is_first)
+    {
+      auto const prefix {(is_first ? "" : "\n") + std::string (indentation, ' ')};
+
+      try
+      {
+        std::rethrow_exception (e);
+      }
+      catch (std::exception const& e)
+      {
+        std::string res {prefix + e.what()};
+
+        try
+        {
+          std::rethrow_if_nested (e);
+        }
+        catch (...)
+        {
+          res += exception_to_string_impl
+            (std::current_exception(), indentation + 1, false);
+        }
+
+        return res;
+      }
+      catch (...)
+      {
+        return prefix + "<unknown exception type>";
+      }
+
+      return prefix + "<no exception>";
+    }
+  }
+
+  std::string exception_to_string (std::exception_ptr ptr)
+  {
+    return exception_to_string_impl (ptr, 0, true);
+  }
+}
