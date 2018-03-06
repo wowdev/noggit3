c36d711133501801f620f60c6ce12a09c942fde0
Adspartan <adspartan04@gmail.com>
2018-03-06 15:00:32 +0100

    multimap_with_normalized_key: remove  non-async multimap

--- src/noggit/multimap_with_normalized_key.hpp
+++ src/noggit/multimap_with_normalized_key.hpp
@@ -17,13 +17,13 @@
 namespace noggit
 {
   template<typename T>
-    struct multimap_with_normalized_key
+    struct async_object_multimap_with_normalized_key
   {
-    multimap_with_normalized_key (std::function<std::string (std::string)> normalize = &mpq::normalized_filename)
+    async_object_multimap_with_normalized_key (std::function<std::string (std::string)> normalize = &mpq::normalized_filename)
       : _normalize (std::move (normalize))
     {}
 
-    ~multimap_with_normalized_key()
+    ~async_object_multimap_with_normalized_key()
     {
       apply ( [&] (std::string const& key, T const&)
               {
@@ -39,10 +39,14 @@ namespace noggit
       std::string const normalized (_normalize (filename));
       if (_counts[normalized]++ == 0)
       {
-        return &_elements.emplace ( std::piecewise_construct
+        T* obj = &_elements.emplace ( std::piecewise_construct
                                   , std::forward_as_tuple (normalized)
                                   , std::forward_as_tuple (normalized, args...)
                                   ).first->second;
+
+        AsyncLoader::instance().queue_for_load(static_cast<AsyncObject*>(obj));
+
+        return obj;
       }
       return &_elements.at (normalized);
     }
@@ -56,16 +60,6 @@ namespace noggit
         _counts.erase (normalized);
       }
     }
-
-    std::size_t count(std::string const& filename)
-    {
-      boost::mutex::scoped_lock lock(_mutex);
-      std::string const normalized (_normalize (filename));
-
-      auto const& it = _counts.find(normalized);
-      return it == _counts.end() ? 0 : it->second;
-    }
-
     void apply (std::function<void (std::string const&, T&)> fun)
     {
       boost::mutex::scoped_lock lock(_mutex);
@@ -88,45 +82,5 @@ namespace noggit
     std::unordered_map<std::string, std::size_t> _counts;
     std::function<std::string (std::string)> _normalize;
     boost::mutex _mutex;
-  };
-
-  template<typename T>
-    struct async_object_multimap_with_normalized_key
-  {
-      async_object_multimap_with_normalized_key (std::function<std::string (std::string)> normalize = &mpq::normalized_filename)
-        : _map(normalize)
-      {}
-
-      template<typename... Args>
-      T* emplace (std::string const& filename, Args&&... args)
-      {
-        T* obj = _map.emplace(filename, args...);
-
-        // new object added
-        if (_map.count(filename) == 1)
-        {
-          AsyncObject* async_obj = static_cast<AsyncObject*>(obj);
-          AsyncLoader::instance().queue_for_load(async_obj);
-        }        
-
-        return obj;
-      }
-
-      void erase (std::string const& filename)
-      {
-        _map.erase(filename);
-      }
-
-      void apply (std::function<void (std::string const&, T&)> fun)
-      {
-        _map.apply(fun);
-      }
-      void apply (std::function<void (std::string const&, T const&)> fun) const
-      {
-        _map.apply(fun);
-      }
-
-  private:
-      multimap_with_normalized_key<T> _map;
   };
 }
