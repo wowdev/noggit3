d67552be080688a7670ea51cad3d690c49518caa
Skarn <skarnproject@gmail.com>
2019-06-09 14:08:47 +0300

    update font awesome to version 5

--- src/noggit/ui/Toolbar.cpp
+++ src/noggit/ui/Toolbar.cpp
@@ -10,7 +10,7 @@ namespace noggit
       : _set_editing_mode (set_editing_mode)
       , _tool_group(this)
     {
-      add_tool_icon (editing_mode::ground,       tr("Raise / Lower"),   font_awesome::areachart);
+      add_tool_icon (editing_mode::ground,       tr("Raise / Lower"),   font_awesome::chartarea);
       add_tool_icon (editing_mode::flatten_blur, tr("Flatten / Blur"),  font_awesome::magic);
       add_tool_icon (editing_mode::paint,        tr("3D Paint"),        font_awesome::paintbrush);
       add_tool_icon (editing_mode::holes,        tr("Holes"),           font_awesome::eraser);
@@ -18,7 +18,7 @@ namespace noggit
       add_tool_icon (editing_mode::flags,        tr("Impassible Flag"), font_awesome::ban);
       add_tool_icon (editing_mode::water,        tr("Water edit"),      font_awesome::tint);
       add_tool_icon (editing_mode::mccv,         tr("Shader editor"),   font_awesome::eyedropper);
-      add_tool_icon (editing_mode::object,       tr("Object editor"),   font_awesome::arrows);
+      add_tool_icon (editing_mode::object,       tr("Object editor"),   font_awesome::arrowsalt);
     }
 
     void toolbar::add_tool_icon(editing_mode mode, const QString& name, const font_awesome::icons& icon)
