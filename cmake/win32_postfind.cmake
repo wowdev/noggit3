eee38e362cc423e2a7cc7307416ac783bfde3c96
Bernd Lörwald <bloerwald@googlemail.com>
2020-08-29 15:20:04 +0200

    cmake: move all-warnings from postfind to main cmake

--- cmake/win32_postfind.cmake
+++ cmake/win32_postfind.cmake
@@ -9,13 +9,6 @@ SET(SourceFiles ${SourceFiles} "${CMAKE_SOURCE_DIR}/include/win/StackWalker.cpp"
 add_compiler_flag_if_supported (CMAKE_CXX_FLAGS /vmg)
 add_compile_definitions (NOMINMAX)
 
-# warnings?
-if( NOGGIT_ALL_WARNINGS )
-    MESSAGE( STATUS "Spilling out mass warnings." )
-    add_compiler_flag_if_supported (CMAKE_CXX_FLAGS /W4)
-    add_compiler_flag_if_supported (CMAKE_CXX_FLAGS /Wall)
-endif( NOGGIT_ALL_WARNINGS )
-
 # mark 32 bit executables large address aware so they can use > 2GB address space
 if(CMAKE_SIZEOF_VOID_P MATCHES 4)
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /LARGEADDRESSAWARE")
