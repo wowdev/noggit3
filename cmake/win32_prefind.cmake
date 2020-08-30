692207ab200c02f795fd92dc1daf7914e99961c5
Bernd Lörwald <bloerwald@googlemail.com>
2020-08-30 18:05:18 +0200

    cmake: move magic library paths to windows specific file

--- cmake/win32_prefind.cmake
+++ cmake/win32_prefind.cmake
@@ -5,6 +5,15 @@
 SET(CMAKE_INCLUDE_PATH "D:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include/" "C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/include/" "C:/Program Files (x86)/StormLib/src/")
 SET(CMAKE_LIBRARY_PATH "D:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/lib/" "C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/lib/" "C:/Program Files (x86)/StormLib/bin/" )
 
+# adds for library repo
+set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} "${CMAKE_SOURCE_DIR}/../Noggit3libs/Boost/lib/")
+#storm lib
+set(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH} "${CMAKE_SOURCE_DIR}/../Noggit3libs/StormLib/include/")
+#boost
+include_directories (SYSTEM "${CMAKE_SOURCE_DIR}/../Noggit3libs/Boost/")
+set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} "${CMAKE_SOURCE_DIR}/../Noggit3libs/Boost/libs/")
+set(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH} "${CMAKE_SOURCE_DIR}/../Noggit3libs/Boost/")
+
 if(LIB_ROOT)
   set(STORMLIB_DIR "${LIB_ROOT}/stormlib")
   set(BOOST_ROOT "${LIB_ROOT}/boost")
