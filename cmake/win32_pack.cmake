00b4ada18f29f3313c9a2f46a7df1dd168f691c9
Bernd Lörwald <bloerwald@googlemail.com>
2020-08-30 18:05:41 +0200

    cmake: install on linux as well, disable broken cpack

--- cmake/win32_pack.cmake
+++ cmake/win32_pack.cmake
@@ -1,14 +1,1 @@
 # This file is part of Noggit3, licensed under GNU General Public License (version 3).
-
-install(TARGETS noggit DESTINATION .)
-install(DIRECTORY bin/shaders DESTINATION .)
-install(DIRECTORY bin/fonts DESTINATION .)
-install(FILES bin/StormLib.dll bin/zlib1.dll DESTINATION .)
-
-set(CPACK_GENERATOR "ZIP;NSIS")
-# \todo Re-add proper version, if we ever use cpack again.
-SET(CPACK_BUNDLE_NAME "Noggit_3")
-SET(CPACK_PACKAGE_FILE_NAME "Noggit")
-SET(CPACK_PACKAGE_VERSION "3")
-
-include(CPack)
