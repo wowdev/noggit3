e4c26d1ef9029beb7c23c9edbe60d9a1f75e44c3
ihm-ts <ihm-ts@protonmail.com>
2021-03-02 12:38:52 +0100

    Fix runtime_error include

--- src/noggit/scripting/script_exception.hpp
+++ src/noggit/scripting/script_exception.hpp
@@ -1,7 +1,7 @@
 // This file is part of Noggit3, licensed under GNU General Public License (version 3).
 #pragma once
 
-#include <exception>
+#include <stdexcept>
 #include <string>
 
 namespace noggit
