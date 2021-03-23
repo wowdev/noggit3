0f218e0586daf530011a284a44c0d5dd5f5b1d35
Bernd Lörwald <bloerwald@googlemail.com>
2021-03-23 01:47:12 +0100

    noggit: scripting: fix: declare context before use

--- src/noggit/scripting/script_context.hpp
+++ src/noggit/scripting/script_context.hpp
@@ -17,6 +17,28 @@ namespace noggit
   {
     class scripting_tool;
 
+    class script_context: public sol::state
+    {
+    public:
+      script_context(scripting_tool * tool);
+      void select_script(int index);
+      int get_selection();
+      World * world();
+      scripting_tool * tool();
+      std::string get_selected_name();
+      std::vector<std::shared_ptr<script_brush>> &get_scripts();
+      sol::table require(std::string const& path);
+      void execute_file(std::string const& filename);
+      std::string file_to_module(std::string const& file);
+      std::string module_to_file(std::string const& module);
+    private:
+      scripting_tool * _tool;
+      std::vector<std::shared_ptr<script_brush>> _scripts;
+      std::map<std::string, sol::table> _modules;
+      std::vector<std::string> _file_stack;
+      int _selected = -1;
+    };
+
     template <typename T>
     class script_scoped_function
     {
@@ -39,28 +61,6 @@ namespace noggit
     private:
       script_context* _lua;
       std::string _name;
-    };
-
-    class script_context: public sol::state
-    {
-    public:
-      script_context(scripting_tool * tool);
-      void select_script(int index);
-      int get_selection();
-      World * world();
-      scripting_tool * tool();
-      std::string get_selected_name();
-      std::vector<std::shared_ptr<script_brush>> &get_scripts();
-      sol::table require(std::string const& path);
-      void execute_file(std::string const& filename);
-      std::string file_to_module(std::string const& file);
-      std::string module_to_file(std::string const& module);
-    private:
-      scripting_tool * _tool;
-      std::vector<std::shared_ptr<script_brush>> _scripts;
-      std::map<std::string, sol::table> _modules;
-      std::vector<std::string> _file_stack;
-      int _selected = -1;
     };
   } // namespace scripting
 } // namespace noggit
