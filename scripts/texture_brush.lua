4be7eb113338db60a5063f5cd8dac5fb12573329
ihm-ts <ihm-ts@protonmail.com>
2021-05-31 12:07:31 +0200

    scripting: improve documentation for texture_brush

--- scripts/texture_brush.lua
+++ scripts/texture_brush.lua
@@ -1,4 +1,10 @@
 local texture_brush = brush("Texture Brush")
+texture_brush:add_description("<b>Description</b>:")
+texture_brush:add_description("- Uses a black / white texture to paint alpha layers. ")
+texture_brush:add_description("- The painting is only additive at the moment,")
+texture_brush:add_description("  meaning it cannot paint layers from \"underneath\"")
+texture_brush:add_description("  another layer.")
+
 local texture = texture_brush:add_string_tag("Brush Texture", "")
 local pressure = texture_brush:add_real_tag("Pressure", 0, 1000, 1, 2)
 local index = texture_brush:add_int_tag("Texture Index", 1, 3, 1)
